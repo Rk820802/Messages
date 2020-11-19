6911fdb96cfc5bd67d6c43fc3b001dcabded1dc1
Rk820802 <70008869+Rk820802@users.noreply.github.com>
2020-11-19 16:21:03 +0100

    Intial commit

---
+++ index.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Home from "./pages/index";
+
+describe("Home", () => {
+  it("renders without crashing", () => {
+    render(<Home />);
+    expect(
+        screen.getByText('ID')
+      ).toBeInTheDocument();
+      expect(
+        screen.getByText('Date')
+      ).toBeInTheDocument();
+  });
+});
+
